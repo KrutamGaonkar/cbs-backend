edcb1257afde13299b56a13c8cfcf4d94c234a8a
LAPTOP-GH5BJAPQ\Krutam <krutam103@gmail.com>
2025-08-15 17:42:11 +0530

    fix port binding for render

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.use(fileUpload()); // to handle image uploads
 
 app.use("/api/articles", articleRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
